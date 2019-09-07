82be880e5ca2af7a300a5f58815e884eba554a01
zhammer <zachary_hammer@alumni.brown.edu>
2019-09-07 14:04:44 -0400

    Update formatting

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import find_packages, setup
 
-with open('README.md', 'r') as readme:
+with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setup(
-    name='tornado-async-transformer',
-    version='0.1.2',
-    description='libcst transformer and codemod for updating tornado @gen.coroutine syntax to python3.5+ native async/await',
-    url='https://github.com/zhammer/tornado-async-transformer',
+    name="tornado-async-transformer",
+    version="0.1.2",
+    description="libcst transformer and codemod for updating tornado @gen.coroutine syntax to python3.5+ native async/await",
+    url="https://github.com/zhammer/tornado-async-transformer",
     packages=find_packages(exclude=["tests", "demo_site"]),
-    package_data={'tornado_async_transformer': ['py.typed']},
+    package_data={"tornado_async_transformer": ["py.typed"]},
     install_requires=["libcst >= 0.1.2"],
-    author='Zach Hammer',
-    author_email='zachary_hammer@alumni.brown.edu',
-    license='MIT License',
+    author="Zach Hammer",
+    author_email="zachary_hammer@alumni.brown.edu",
+    license="MIT License",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries",
