6d1be5e2c7e557fd49c2759588ceddc608bf6711
zhammer <zachary_hammer@alumni.brown.edu>
2019-09-07 12:57:49 -0400

    Remove TODO

--- tornado_native_async_transformer/tornado_native_async_transformer.py
+++ tornado_native_async_transformer/tornado_native_async_transformer.py
@@ -22,7 +22,6 @@ class TornadoNativeAsyncTransformer(cst.CSTTransformer):
     """
 
     # TODO: @tornado.gen.coroutine, @tornado.gen.Return
-    # TODO: gen.sleep
 
     def __init__(self):
         self.coroutine_stack: List[bool] = []
